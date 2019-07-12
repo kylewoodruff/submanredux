6d0d42ba55ce349d8097badf1fabe71a5465cd52
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-07-12 00:36:26 -0700

    landing page with styled components

---
+++ client/src/components/Jumbotron-Landing/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Jumbo = styled.div`
+    margin-top: 225px;
+    background-color: rgba(0, 0, 0, 0.0);
+    padding: 35px;
+    border-radius: 15px;
+    font-family: 'Roboto', sans-serif;
+
+    @media (max-width: 576px) {
+        margin-top: 135px;
+    }
+`;
+
+const Slogan = styled.h1`
+    color: white;
+    font-size: 48px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bolder;
+`;
+
+const SubSlogan = styled.p`
+    color: white; 
+    font-size: 18px;
+    margin-top: 20px;
+`;
+
+const User = styled.p`
+    font-size: 16px;
+    color: white;
+    margin-top: 25px;
+`;
+
+const JumboSign = styled.a`
+    color: white;
+    font-size: 16px;
+    margin-top: -25px;
+
+    :hover {
+        color: rgb(39, 111, 219);
+        text-decoration: none;
+    }
+`;
+
+const SignUp = styled.a`
+    margin: 10px;
+    margin-top: 30px;
+    font-size: 28px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    background-color: rgba(37, 101, 221);
+    color: white;
+    padding: 15px 35px 15px 35px;
+    border-radius: 10px;
+
+    :hover {
+        color: rgb(0, 174, 255);
+        text-decoration: none;
+    }
+`;
+
+function Jumbotron() {
+    return (
+        <Jumbo>
+            <Slogan className="text-center">Diving deep to keep your subsciptions neat.</Slogan>
+            <SubSlogan className="text-center">All of your subscriptions in one place.</SubSlogan>
+            <div className="text-center">
+                <SignUp className="btn-lg" href="/" role="button">Sign Up Now!</SignUp>
+                    <User className="text-center user">Already a user?</User>
+                    <JumboSign className="btn btn-sm jumboSign" href="/"
+                        role="button">Login</JumboSign>
+            </div>
+        </Jumbo>
+    )
+}
+
+export default Jumbotron;
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron-Landing";
+import LandNav from "../components/LandNav";
+import Wrapper from "../components/Wrapper";
+import "../assets/landing.css"
+
+
+class Landing extends Component {
+    render() {
+        return (
+            <div>
+                <LandNav />
+                <div className="row">
+                    <div className="col-sm-0 col-lg-1 col-xl-1">
+                        <p></p>
+                    </div>
+                    <div className="col-sm-12 col-lg-10 col-xl-10">
+                        <Wrapper>
+                            <Jumbotron />
+                        </Wrapper>
+                    </div>
+                    <div className="col-sm-0 col-lg-1 col-xl-1">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Landing;
+
