f56f4272dfe4210b28c8702ad0d2710d61c4972d
George Shpak <georgeshpak@Georges-MacBook-Air.local>
2019-07-24 07:37:03 -0700

    update domain

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -3,7 +3,7 @@ import auth0 from 'auth0-js';
  class Auth {
    constructor(){
         this.auth0 = new auth0.WebAuth({
-            domain: "sub-merge.auth0.com",
+            domain: "https://sub-merge.auth0.com",
             clientID: process.env.REACT_APP_CLIENTID||"teNlTbyVB3lCq5OTWhmxEbkLJlLowDJN",
             redirectUri: process.env.REACT_APP_URL||"http://localhost:3000/callback",
             audience: "https://sub-merge.auth0.com/userinfo",
