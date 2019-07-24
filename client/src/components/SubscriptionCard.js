6f11ae333c3f8e72f09645a1e82051eea3758f4c
Kyle Woodruff <kylewoodruff@gmail.com>
2019-07-23 19:25:30 -0700

    Basic designs

---
+++ client/src/components/SubscriptionCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Card = styled.div`
+    width: 18rem;
+    
+`;
+
+function SubscriptionCard() {
+    return (
+        <Card class="card">
+            <div class="card-body d-flex justify-content-around">
+                <h5 class="card-title">Subscription Name</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Due: 8/13/2019</h6>
+                <p class="card-text">$17.99</p>
+            </div>
+        </Card> 
+
+    )
+}
+export default SubscriptionCard;
--- client/src/pages/Subscriptions.js
+++ client/src/pages/Subscriptions.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import Navbar from "../components/Navbar.js";
 import Header from "../components/Header.js";
 import { createGlobalStyle } from 'styled-components';
+import SubscriptionCard from "../components/SubscriptionCard";
+import Wrapper from "../components/Wrapper"; 
+import Wrapper2 from "../components/Wrapper2.js";
 
 const GlobalStyle = createGlobalStyle`
   body {
     color: ${props => (props.whiteColor ? 'white' : 'black')};
+    background:  black;
   }
   `
 
@@ -14,9 +18,25 @@ class Subscriptions extends Component {
     return (
       <React.Fragment>
         <GlobalStyle whiteColor />
-        <Header />
-        <h1>Hello World!!!</h1>
-        <Navbar />
+        <div className="row">
+          <Header />
+        </div>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <Wrapper2>
+          <Navbar />
+          <Wrapper>
+
+
+            <div className="container-fluid ml-5">
+              <h4 className="text-left ml-5">Subscriptions</h4>
+              <SubscriptionCard />
+            </div>
+          </Wrapper>
+        </Wrapper2>
+
       </React.Fragment>
     )
   }
