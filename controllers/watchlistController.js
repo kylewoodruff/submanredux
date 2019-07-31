16bea4b1231d2c52faa66dacbcdac80e30832904
Kyle Woodruff <kylewoodruff@gmail.com>
2019-07-30 17:17:54 -0700

    fixed the get call for watchlists

--- controllers/watchlistController.js
+++ controllers/watchlistController.js
@@ -3,8 +3,7 @@ const db = require('../models');
 module.exports = {
   findAll(req, res) {
     db.User
-      .find(req.query)
-      .sort({ date: -1 })
+      .find({ id: req.user.sub })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
